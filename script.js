99521b1f50efdc22d254c36574670c1cfa3eba8b
Andrey Belyun <andreibelyun@yandex.com>
2021-06-26 18:13:45 +0300

    Finished project

---
+++ script.js
@@ -0,0 +1,26 @@
+let editProfileButton = document.querySelector('.profile__edit');
+let popupElement = document.querySelector('.popup');
+let popupInputNameElement = document.querySelector('.popup__input_type_name');
+let popupInputInfoElement = document.querySelector('.popup__input_type_description');
+let popupCloseButton = popupElement.querySelector('.popup__close');
+let popupSaveButtonElement = popupElement.querySelector('.popup__save');
+
+function initForm() {
+    popupInputNameElement.value = document.querySelector('.profile__name').textContent;
+    popupInputInfoElement.value = document.querySelector('.profile__description').textContent;
+}
+
+editProfileButton.addEventListener('click', function() {
+    popupElement.classList.add('popup_opened');
+    initForm();
+});
+
+popupCloseButton.addEventListener('click', function() {
+    popupElement.classList.remove('popup_opened');
+});
+
+popupSaveButtonElement.addEventListener('click', function() {
+    document.querySelector('.profile__name').textContent = popupInputNameElement.value;
+    document.querySelector('.profile__description').textContent = popupInputInfoElement.value;
+    popupElement.classList.remove('popup_opened');
+});
